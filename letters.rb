190b7cb78d9add90f093326bc3cb88c4f8884395
Jace P. Gold <Jace@JaceGold.com>
2017-12-28 21:13:06 -0700

    Functionality complete

--- letters.rb
+++ letters.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
-def letters_menu
-  puts "\n------Letters Menu------"
+def letters
+  puts "\n------Welcome to Letters!------"
   puts "1) Enter input"
   puts "2) Exit"
   user_input = get_input.to_i
@@ -11,76 +11,60 @@ def letters_menu
       user_string = get_input
       puts "You put #{user_string}"
       check_input(user_string)
-      letters_menu
+      letters
     elsif user_input == 2
       puts "Thanks for using my program."
       exit
     else
       puts "I'm sorry, there was some kind of issue. Please try again."
-      letters_menu
+      letters
     end
   elsif user_input < 1
     puts "I'm sorry, your number was too low."
     puts "Please try again."
-    letters_menu
+    letters
   elsif user_input > 2
     puts "I'm sorry, your number was too high."
     puts 'Please try again'
-    letters_menu
+    letters
   else
     puts "I'm sorry, please try again"
-    letters_menu
+    letters
   end
 end
 
-def check_input(user_string)
-  has_multiple = false
-  mulitple_count = 0
-  puts "Entered check input with #{user_string}"
-  input_array = user_string.split(" ")
-
-
-  for item in input_array
-    puts item
-    letters = item.downcase.gsub(/[^a-z]/, '').chars
-    binding.pry
-    if letters == letters.uniq
-      puts letters
+def check_input(string)
+  # If you get 0 or 1 that means it doesn't repeat so we start at 2
+  highest_repeated = 2
+  highest = []
+  string.split(' ').each do |word|
+    occurences = {}
+    word.each_char do |char|
+      occurences.has_key?(char) ? occurences[char] += 1 : occurences[char] = 1
     end
-
-  end
-
-
-  # s = "The quick brown fox jumps over the lazy dog."
-  # .downcase
-  # ("a".."z").all?{|c| s.count(c) <= 1}
-  # # => false
-
-
-  if has_multiple
-    puts "Mulitple duplicate letters found."
-    if multiple_count == 1
-      puts "Highest repeated letters in the word: <word here>"
-      letters_menu
-    else
-      puts "Multiples Found: "
-      letters_menu
+    high = occurences.sort_by{|key, value| value}.reverse.first.last
+    if high == highest_repeated
+      # found multiples so push onto the array
+      highest_repeated = high
+      highest << word
+    elsif high > highest_repeated
+      # found one with higher repeat count than what was set so we reset the array
+      highest_repeated = high
+      highest = [word]
     end
-  else 
-    puts "No multiple letters found in any words."
-    letters_menu
   end
+  if highest.length >= 2
+    puts "Multiple Have Been Found: #{highest.join(', ')}"
+  elsif highest.length == 1
+    puts "The Highest repeated letters is in the word: #{highest.join(',')}"
+  else
+    puts 'No multiple letters found in any word...'
+  end  
 end
 
 def get_input
   print '> '
   user_input = $stdin.gets.strip;
-end
-
-def letters
-  puts "------Welcome to Letters------"
-  letters_menu
-
 end
 
 letters
